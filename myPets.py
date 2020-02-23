fb88889b75d2b03942d302bdb985b5a66103304c
Tomas Quinones <tomas.quinones@gmail.com>
2020-02-23 13:56:57 -0800

    more random

--- myPets.py
+++
@@ -1,9 +0,0 @@
-#myPets.py
-
-myPets = ['Zophie', 'Pooka', 'Fat-tail']
-print('Enter a pet name:')
-name = input()
-if name not in myPets:
-    print('I do not have a pet named ' + name)
-else:
-    print(name + ' is my pet.')
